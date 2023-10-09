25db5439e02e453acb20b8d2ed3b85369c170169
BlackdBlook <zhangshihao2456@yahoo.com>
2023-10-09 17:00:38 +0800

    准备增加导入模型支持

--- Engine/Component/Mesh/Mesh.h
+++ Engine/Component/Mesh/Mesh.h
@@ -1,9 +1,23 @@
 #pragma once
 #include "../Component.h"
 #include <vector>
+#include "Header.h"
+#include "Engine/Core/Vertex/Vertex.h"
+
+
 class Mesh :public Component
 {
 public:
-    Mesh(std::vector<float> vertices);
-};
-
+    /*  网格数据  */
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
+    /*  函数  */
+    Mesh(std::vector<Vertex>&& vertices, std::vector<unsigned int>&& indices, std::vector<Texture>&& textures);
+    void Draw(const ShaderProgram& shader);
+private:
+    /*  渲染数据  */
+    unsigned int VAO, VBO, EBO;
+    /*  函数  */
+    void setupMesh();
+};  
